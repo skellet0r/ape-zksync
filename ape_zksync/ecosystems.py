92bbec08a709255dda31297baf3fe05842d3407c
Edward Amor <edward.amor3@gmail.com>
2022-08-06 14:41:16 +0000

    feat: add ZKSync ecosystem class

--- ape_zksync/ecosystems.py
+++ ape_zksync/ecosystems.py
@@ -1,6 +1,17 @@
 from ape.api import BlockAPI
+from ape_ethereum.ecosystem import Ethereum
 from pydantic import Field
+
+from ape_zksync.config import ZKSyncConfig
 
 
 class ZKSyncBlock(BlockAPI):
     size: None = Field(None, exclude=True, repr=False)
+
+
+class ZKSync(Ethereum):
+    name = "zksync"
+
+    @property
+    def config(self) -> ZKSyncConfig:
+        return self.config_manager.get_config("zksync")
