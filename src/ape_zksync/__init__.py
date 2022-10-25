eab9daa56832b39268787099fd242e33b5e7cee2
Edward Amor <edward.amor3@gmail.com>
2022-10-25 04:38:19 -0400

    fix: register zkvyper compiler

--- src/ape_zksync/__init__.py
+++ src/ape_zksync/__init__.py
@@ -1,6 +1,8 @@
 from ape import plugins
 from ape.api import PluginConfig
 
+from ape_zksync.compiler import ZKVyperCompiler
+
 
 class ZKSyncConfig(PluginConfig):
     ...
@@ -9,3 +11,8 @@ class ZKSyncConfig(PluginConfig):
 @plugins.register(plugins.Config)
 def config_class():
     return ZKSyncConfig
+
+
+@plugins.register(plugins.CompilerPlugin)
+def register_compiler():
+    return (".zkvy",), ZKVyperCompiler
