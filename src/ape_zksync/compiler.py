98865e610db0a7d2a06264b27b26e832ad545be8
Edward Amor <edward.amor3@gmail.com>
2022-10-25 04:14:37 -0400

    fix: add get_version_map method

--- src/ape_zksync/compiler.py
+++ src/ape_zksync/compiler.py
@@ -1,10 +1,12 @@
 import contextlib
 import re
+from collections import defaultdict
 from pathlib import Path
-from typing import List, Set
+from typing import Dict, List, Optional, Set
 
+import zkvvm
 from ape.api import CompilerAPI
-from semantic_version import NpmSpec
+from semantic_version import SimpleSpec, Version
 
 
 class ZKVyperCompiler(CompilerAPI):
@@ -20,5 +22,31 @@ class ZKVyperCompiler(CompilerAPI):
             for file in files:
                 mo = pattern.match(file.readline())
                 with contextlib.suppress(ValueError):
-                    versions.append(str(NpmSpec(mo.group(1) if mo else "latest")))
+                    versions.append(str(SimpleSpec(mo.group(1) if mo else "latest")))
         return set(versions)
+
+    def get_version_map(
+        self, contract_filepaths: List[Path], base_path: Optional[Path] = None
+    ) -> Dict[Version, Set[Path]]:
+        version_specs = [SimpleSpec(v) for v in self.get_versions(contract_filepaths)]
+        version_manager = zkvvm.VersionManager(zkvvm.Config())
+        for spec in version_specs:
+            if spec.select(version_manager.local_versions):
+                continue
+            selected = spec.select(version_manager.remote_versions)
+            if not selected:
+                raise ValueError(f"No zkVyper version meeting constraint: {spec!s}")
+            version_manager.install(selected, show_progress=True)
+
+        pattern = re.compile(r"\s* # \s+ @zk-version \s+ (.+)", re.VERBOSE)
+        version_map = defaultdict(set)
+        with contextlib.ExitStack() as stack:
+            for fp in contract_filepaths:
+                mo = pattern.match(stack.enter_context(fp.open()).readline())
+                try:
+                    spec = SimpleSpec(mo.group(1) if mo else ">=0.1.0")
+                except ValueError:
+                    spec = SimpleSpec(">=0.1.0")
+                version = spec.select(version_manager.local_versions)
+                version_map[version].add(fp)
+        return version_map
