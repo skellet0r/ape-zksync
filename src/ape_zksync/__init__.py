8eda6e42046d8035eca3580c2a760652f8ea4d41
Edward Amor <edward.amor3@gmail.com>
2022-10-27 05:11:49 -0400

    fix: add zksync ecosystem, provider + network plugins

--- src/ape_zksync/__init__.py
+++ src/ape_zksync/__init__.py
@@ -1,7 +1,9 @@
 from ape import plugins
+from ape.api import create_network_type
 
 from ape_zksync.compiler import ZKVyperCompiler
 from ape_zksync.config import ZKSyncConfig
+from ape_zksync.ecosystem import ZKSync, ZKSyncProvider
 
 
 @plugins.register(plugins.Config)
@@ -12,3 +14,18 @@ def config_class():
 @plugins.register(plugins.CompilerPlugin)
 def register_compiler():
     return (".zkvy",), ZKVyperCompiler
+
+
+@plugins.register(plugins.EcosystemPlugin)
+def ecosystems():
+    yield ZKSync
+
+
+@plugins.register(plugins.NetworkPlugin)
+def networks():
+    yield "zksync", "testnet", create_network_type(270, 270)
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    yield "zksync", "testnet", ZKSyncProvider
