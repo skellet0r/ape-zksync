cc30e11b856098101a635f9c58eb20ce842b4f84
Edward Amor <edward.amor3@gmail.com>
2022-10-27 05:03:55 -0400

    fix: add override methods

--- src/ape_zksync/ecosystem.py
+++ src/ape_zksync/ecosystem.py
@@ -1,5 +1,21 @@
-from ape.api import BlockAPI
+from hashlib import sha256
+from typing import Dict
+
+from ape.api import BlockAPI, TransactionAPI
+from ape.utils import EMPTY_BYTES32
+from ape_ethereum.ecosystem import Ethereum
+from ethpm_types.abi import ConstructorABI
+from hexbytes import HexBytes
 from pydantic import Field, validator
+
+from ape_zksync.config import ZKSyncConfig
+from ape_zksync.constants import CONTRACT_DEPLOYER, CONTRACT_DEPLOYER_TYPE
+from ape_zksync.transaction import (
+    LegacyTransaction,
+    TransactionType,
+    ZKSyncReceipt,
+    ZKSyncTransaction,
+)
 
 
 class ZKSyncBlock(BlockAPI):
@@ -11,3 +27,72 @@ class ZKSyncBlock(BlockAPI):
     @validator("l1_batch_number", pre=True)
     def to_int(self, value):
         return int(value, 16)
+
+
+class ZKSync(Ethereum):
+    name = "zksync"
+
+    @property
+    def config(self) -> ZKSyncConfig:
+        return super(Ethereum, self).config
+
+    def serialize_transaction(self, transaction: TransactionAPI) -> bytes:
+        return transaction.serialize_transaction()
+
+    def decode_block(self, data: Dict) -> BlockAPI:
+        data["num_transactions"] = len(data["transactions"])
+        return ZKSyncBlock.parse_obj(data)
+
+    def decode_receipt(self, data: dict) -> ZKSyncReceipt:
+        receipt = ZKSyncReceipt.parse_obj(data)
+        deployment = next(
+            (
+                log
+                for log in receipt.decode_logs()
+                if log.contract_address == CONTRACT_DEPLOYER
+                and log.event_name == "ContractDeployed"
+            ),
+            None,
+        )
+        if deployment:
+            receipt.contract_address = deployment
+        return receipt
+
+    def encode_deployment(
+        self, deployment_bytecode: HexBytes, abi: ConstructorABI, *args, **kwargs
+    ) -> ZKSyncTransaction:
+        # contract deployments require a tx to the Contract Deployer contract
+        create_abi = CONTRACT_DEPLOYER_TYPE.mutable_methods["create"]
+
+        # bytecodehash passed as an argument is the sha256 hash of the
+        # init code, where the upper 2 bytes are the word length of the init code
+        bytecode_hash = sha256(
+            deployment_bytecode
+        ).hexdigest()  # doesn't have leading 0x
+        bytecode_hash = (
+            "0x" + hex(len(deployment_bytecode) // 32)[2:].zfill(4) + bytecode_hash[4:]
+        )
+        create_args = [
+            HexBytes(EMPTY_BYTES32),
+            HexBytes(bytecode_hash),
+            kwargs["value"],
+            self.encode_calldata(abi, *args) if abi.inputs else b"",
+        ]
+
+        # modify kwargs
+        kwargs["type"] = 0x71
+        kwargs["factory_deps"] = [deployment_bytecode] + kwargs.get("factory_deps", [])
+        kwargs.setdefault("gas_price", self.provider.gas_price)
+        kwargs["chain_id"] = self.provider.chain_id
+
+        return super().encode_transaction(
+            CONTRACT_DEPLOYER, create_abi, *create_args, **kwargs
+        )
+
+    def create_transaction(self, **kwargs) -> TransactionAPI:
+        if kwargs["type"] == TransactionType.LEGACY:
+            return LegacyTransaction.parse_obj(kwargs)
+        elif kwargs["type"] == TransactionType.ZKSYNC:
+            return ZKSyncTransaction.parse_obj(kwargs)
+
+        raise Exception()
