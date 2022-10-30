e036c5b829f0e1daf19fbc246f748ac63df0ecdc
Edward Amor <edward.amor3@gmail.com>
2022-10-30 01:20:44 -0400

    fix: hash_bytecode utility

--- src/ape_zksync/utils.py
+++ src/ape_zksync/utils.py
@@ -1,16 +1,18 @@
 import hashlib
 from typing import Any
 
+from eth_typing import HexStr
 from hexbytes import HexBytes
 
 
-def hash_bytecode(bytecode: bytes) -> bytes:
+def hash_bytecode(bytecode: HexStr) -> bytes:
     """zkSync deployment bytecode hash function.
 
     :param bytes bytecode: The contract deployment bytecode.
     :returns: The bytecode hash for use during contract creation.
     :rtype: bytes
     """
+    bytecode = HexBytes(bytecode)
     bytecode_hash = hashlib.sha256(bytecode).digest()
     word_length = (len(bytecode) // 32).to_bytes(2, "big")
     return b"\x01\x00" + word_length + bytecode_hash[4:]
