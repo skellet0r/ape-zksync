82bccc6168d404849d2d1509d12b50ac8eb3f848
Edward Amor <edward.amor3@gmail.com>
2022-10-29 23:40:12 -0400

    fix: add to_bytes utility function

--- src/ape_zksync/utils.py
+++ src/ape_zksync/utils.py
@@ -1,4 +1,7 @@
 import hashlib
+from typing import Any
+
+from hexbytes import HexBytes
 
 
 def hash_bytecode(bytecode: bytes) -> bytes:
@@ -11,3 +14,19 @@ def hash_bytecode(bytecode: bytes) -> bytes:
     bytecode_hash = hashlib.sha256(bytecode).digest()
     word_length = (len(bytecode) // 32).to_bytes(2, "big")
     return b"\x01\x00" + word_length + bytecode_hash[4:]
+
+
+def to_bytes(value: Any, lstrip: bool = True) -> HexBytes:
+    """Convert a value to bytes.
+
+    :param value: A value to convert.
+    :param bool lstrip: Remove leading null bytes from the output.
+    :returns: A bytes value.
+    :rtype: HexBytes
+    """
+    if not value:
+        return HexBytes(b"")
+    elif value and lstrip:
+        return HexBytes(value).lstrip(b"\x00")
+    else:
+        return HexBytes(value)
